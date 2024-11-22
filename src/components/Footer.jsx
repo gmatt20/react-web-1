fbee73bc27ba9ae523c16131f0aae55a843c1857
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-21 20:19:04 -0800

    Added padding to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
   return(
-    <footer className="text-5xl bg-amber-500">
+    <footer className="text-5xl bg-amber-500 p-10">
       <h1>
       Hello! I am the footer.
       </h1>
