df9186af7f4d1cf6ccbd07f6aa0b46e8c256deb0
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-22 21:39:01 -0800

    Added link to from react router dom!

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () =>{
   return(
-    <nav className = "grid grid-flow-col items-center">
+    <nav className = "grid grid-flow-col items-center border-b-2 border-rose-200 pb-2">
       <h1 className="text-rose-500 font-semibold text-3xl">Luna Blog</h1>
       <div className="grid grid-flow-col justify-end gap-5 ">
-        <a href="/" className="hover:text-rose-500 transition ease-in-out delay-100">Home</a>
-        <a href="/create" className="hover:text-rose-500 transition ease-in-out delay-100">New Blog</a>
+        <Link to="/" className="hover:text-rose-500 transition ease-in-out delay-100">Home</Link>
+        <Link to="/create" className="hover:text-rose-500 transition ease-in-out delay-100">New Blog</Link>
       </div>
     </nav>
   );
