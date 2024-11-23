19990a21f13d6924d151858648d398c2fb70cb7f
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-23 08:27:29 -0800

    Added delete functionality

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,18 @@
 import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id,{
+      method: 'DELETE'
+    }).then(()=>{
+      history.push('/');
+    })
+  }
   return(
     <div className="mt-20">
       { isPending && <div>Loading...</div>}
@@ -13,6 +22,7 @@ const BlogDetails = () => {
           <h2 className="text-2xl font-semibold mt-10 text-rose-500 mb-1">{ blog.title }</h2>
           <p className="mb-5 font-semibold">Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button className="bg-rose-500 text-white border-0 my-5 p-2 rounded-md cursor-pointer" onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
